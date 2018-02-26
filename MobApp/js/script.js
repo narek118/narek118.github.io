547e8d1ac0faa413e6fbb18efb6883f5c9706a23
Narek <narek.harutyunyan@eprintwerx.com>
2018-02-26 16:11:46 +0400

    site added

---
+++ MobApp/js/script.js
@@ -0,0 +1,177 @@
+/*
+
+Style   : MobApp Script JS
+Version : 1.0
+Author  : Surjith S M
+URI     : https://surjithctly.in/
+
+Copyright © All rights Reserved 
+
+*/
+
+
+$(function() {
+    "use strict";
+
+    /*-----------------------------------
+     * FIXED  MENU - HEADER
+     *-----------------------------------*/
+    function menuscroll() {
+        var $navmenu = $('.nav-menu');
+        if ($(window).scrollTop() > 50) {
+            $navmenu.addClass('is-scrolling');
+        } else {
+            $navmenu.removeClass("is-scrolling");
+        }
+    }
+    menuscroll();
+    $(window).on('scroll', function() {
+        menuscroll();
+    });
+    /*-----------------------------------
+     * NAVBAR CLOSE ON CLICK
+     *-----------------------------------*/
+
+    $('.navbar-nav > li:not(.dropdown) > a').on('click', function() {
+        $('.navbar-collapse').collapse('hide');
+    });
+    /* 
+     * NAVBAR TOGGLE BG
+     *-----------------*/
+    var siteNav = $('#navbar');
+    siteNav.on('show.bs.collapse', function(e) {
+        $(this).parents('.nav-menu').addClass('menu-is-open');
+    })
+    siteNav.on('hide.bs.collapse', function(e) {
+        $(this).parents('.nav-menu').removeClass('menu-is-open');
+    })
+
+    /*-----------------------------------
+     * ONE PAGE SCROLLING
+     *-----------------------------------*/
+    //Select all links with hashes
+    
+    $('a[href*="#"]').not('[href="#"]').not('[href="#0"]').not('[data-toggle="tab"]').on('click', function(event) {
+        // On-page links
+        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+            // Figure out element to scroll to
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+            // Does a scroll target exist?
+            if (target.length) {
+                // Only prevent default if animation is actually gonna happen
+                event.preventDefault();
+                $('html, body').animate({
+                    scrollTop: target.offset().top
+                }, 1000, function() {
+                    // Callback after animation
+                    // Must change focus!
+                    var $target = $(target);
+                    $target.focus();
+                    if ($target.is(":focus")) { // Checking if the target was focused
+                        return false;
+                    } else {
+                        $target.attr('tabindex', '-1'); // Adding tabindex for elements not focusable
+                        $target.focus(); // Set focus again
+                    };
+                });
+            }
+        }
+
+});
+    /*-----------------------------------
+     * OWL CAROUSEL
+     *-----------------------------------*/
+    var $testimonialsDiv = $('.testimonials');
+    if ($testimonialsDiv.length && $.fn.owlCarousel) {
+        $testimonialsDiv.owlCarousel({
+            items: 1,
+            nav: false,
+            dots: false,
+            navText: ['<span class="ti-arrow-left"></span>', '<span class="ti-arrow-right"></span>']
+        });
+    }
+
+    var $galleryDiv = $('.img-gallery');
+    if ($galleryDiv.length && $.fn.owlCarousel) {
+        $galleryDiv.owlCarousel({
+            nav: false,
+            center: true,
+            loop: true,
+            autoplay: true,
+            dots: true,
+            navText: ['<span class="ti-arrow-left"></span>', '<span class="ti-arrow-right"></span>'],
+            responsive: {
+                0: {
+                    items: 1
+                },
+                768: {
+                    items: 3
+                }
+            }
+        });
+    }
+
+    $('.translate').click(function(){
+        $('.translate').removeClass('rem');
+        $(this).addClass('rem');
+    });
+
+jQuery(document).ready(function($) {
+  $('.collapsed-block').click(function(e) {
+    $(this).toggleClass('toggled');
+  });
+});
+
+}); /* End Fn */
+
+
+
+    $(".navbar-toggler-icon").click(function(){
+    $(".navbar-toggler-icon").css("display", "none");
+    $(".navbar-toggler-icon_2").css("display", "block");
+    $(".nav-menu").css({"background": "-webkit-linear-gradient(top, rgb(253, 253, 255) 25%, rgba(255, 255, 255, 0.95) 25%)"});
+});
+$(".navbar-toggler-icon_2").click(function(){
+      $(".navbar-toggler-icon").css("display", "initial");
+      $(".navbar-toggler-icon_2").css("display", "none");
+      $(".nav-menu").css({"background": "#fff"});
+});
+$(".nav-link").click(function(){
+     $(".navbar-toggler-icon").css("display", "initial");
+      $(".navbar-toggler-icon_2").css("display", "none");
+      $(".nav-menu").css({"background": "#fff"});
+});
+
+if ( $(window).width() < 1024 ) {
+    $("#langs").appendTo('#lang-append');
+}
+
+
+$( window ).resize(function() {
+  if ( $(window).width() < 1024 ) {
+    $("#langs").appendTo('#lang-append');
+    
+}
+
+else {
+    $("#langs").appendTo('#langs');
+}
+});
+
+/*arrow top scroll start*/
+
+$(window).scroll(function() {
+    if ($(this).scrollTop() >= 500) {        // If page is scrolled more than 50px
+        $('#return-to-top').fadeIn(200);    // Fade in the arrow
+    } else {
+        $('#return-to-top').fadeOut(200);   // Else fade out the arrow
+    }
+});
+$('#return-to-top').click(function() {      // When arrow is clicked
+    $('body,html').animate({
+        scrollTop : 0                       // Scroll to top of body
+    }, 500);
+});
+
+/*arrow top scroll end*/
